888ff340e2d14ec56ebc67d5f13245c05ef2725b
eightys3v3n <eightys3v3n@gmail.com>
2021-04-14 10:07:39 -0600

    added erase flash command

--- code/build.sh
+++ code/build.sh
@@ -1,2 +1,5 @@
 git config advise.detachedHead false
+
+# if you get unprecidented crashes try this,
+#esptool.py --port /dev/ttyUSB1 erase_flash
 pio run -e plant_water && esptool.py --baud 256000 --after hard_reset --chip esp8266 write_flash 0 .pio/build/plant_water/firmware.bin
