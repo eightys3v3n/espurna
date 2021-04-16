b8ca7ee5031a2931b8a340d0d021ee6749c087a6
eightys3v3n <eightys3v3n@gmail.com>
2021-04-16 08:25:04 -0600

    changed to custom build profile instead of plant_water

--- code/build.sh
+++ code/build.sh
@@ -1,5 +1,7 @@
+pushd ..
 git config advise.detachedHead false
+popd
 
 # if you get unprecidented crashes try this,
 #esptool.py --port /dev/ttyUSB1 erase_flash
-pio run -e plant_water && esptool.py --baud 256000 --after hard_reset --chip esp8266 write_flash 0 .pio/build/plant_water/firmware.bin
+pio run -e custom && esptool.py --baud 256000 --after hard_reset --chip esp8266 write_flash 0 .pio/build/custom/firmware.bin
