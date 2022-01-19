9c7704039cb4f8e81e275a19db4d0eb8d5a8341c
eightys3v3n <eightys3v3n@gmail.com>
2022-01-19 15:25:54 -0700

    added flash command

--- code/terrences_build.sh
+++ code/terrences_build.sh
@@ -1,2 +1,2 @@
 git config advise.detachedHead false
-pio run -e s31_outlet
+pio run -e s31_outlet && esptool.py --baud 256000 --after hard_reset --chip esp8266 write_flash 0 .pio/build/s31_outlet/firmware.bin
